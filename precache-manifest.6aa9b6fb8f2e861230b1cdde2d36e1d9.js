c428ac0557141285bbacfb14f3ca080a4bd9694b
Sergi <ocasergi@gmail.com>
2019-09-25 01:33:34 +0200

    Add files via upload

---
+++ precache-manifest.6aa9b6fb8f2e861230b1cdde2d36e1d9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "80d83825d563eea9e54e",
+    "url": "/js/chunk-vendors.1b6195e0.js"
+  },
+  {
+    "revision": "b2f3ccc4246493d9f755",
+    "url": "/js/app.89fc9c34.js"
+  },
+  {
+    "revision": "525733a04935fd8be85b93aaa8a7a7f3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "80d83825d563eea9e54e",
+    "url": "/css/chunk-vendors.c71b18f7.css"
+  },
+  {
+    "revision": "b2f3ccc4246493d9f755",
+    "url": "/css/app.2c88bf33.css"
+  }
+];
